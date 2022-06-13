5f8744a063ed615529c844b581b0917892684cf6
Dong Min Kim <87836793+dmkim22-lguplus@users.noreply.github.com>
2022-06-13 11:23:32 +0900

    test

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+var config = require('./config/config.json')
 
 // Constants
 const PORT = 8080
 const HOST = '0.0.0.0'
+
+const db_user = config.get(db_user)
+const db_user = config.get(db_password)
 
 // App
 const app = express()
