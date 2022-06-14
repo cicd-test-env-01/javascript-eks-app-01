ba3a5f79b5cdd229644411ed02c6ad1fed3d5eab
Dong Min Kim <87836793+dmkim22-lguplus@users.noreply.github.com>
2022-06-14 14:37:22 +0900

    revert to no kms

--- server.js
+++ server.js
@@ -1,35 +1,13 @@
-// Constants
+// Module
 const express = require('express')
 const config = require('config')
 
-const DecryptCommand = require('@aws-sdk/client-kms')
-
-const keyIds = [
-  'arn:aws:kms:ap-northeast-2:521651615177:key/6ca496ce-938b-4038-af90-e1614da7535c',
-]
-
+// Constants
 const PORT = 8080
 const HOST = '0.0.0.0'
 
 const dbUser = config.get('Database.dbUser')
 const dbPassword = config.get('Database.dbPassword')
-const dbPasswordEncrypted = config.get('Database.dbPassword')
-
-const decryptInput = {
-  CiphertextBlob: dbPasswordEncrypted,
-  EncryptionAlgorithm: 'RSAES_OAEP_SHA_256',
-  KeyId: keyIds,
-}
-
-async function decrypt() {
-  // const client = new KMSClient(config)
-  const command = new DecryptCommand(decryptInput)
-  const dbPasswordPlain = await client.send(command)
-
-  alert(dbPasswordPlain)
-}
-
-const dbPasswordPlain = decrypt()
 
 // App
 const app = express()
@@ -40,6 +18,4 @@ app.get('', (req, res) => {
 })
 
 app.listen(PORT, HOST)
-console.log(
-  `DB : ${dbUser} ${dbPassword} ${dbPasswordPlain} Running on http://${HOST}:${PORT}`
-)
+console.log(`DB : ${dbUser} ${dbPassword} Running on http://${HOST}:${PORT}`)
