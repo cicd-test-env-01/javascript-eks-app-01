6d82ee7abdb3ae30741d872b649c438f517ea61c
Dong Min Kim <87836793+dmkim22-lguplus@users.noreply.github.com>
2022-06-15 10:53:42 +0900

    external config test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/KMS.html#decrypt-property
+
+https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/index.html
+https://stackoverflow.com/questions/56827993/decrypt-text-with-aws-kms-in-nodejs
--- server.js
+++ server.js
@@ -1,36 +1,32 @@
-// Module
 const express = require('express')
-const config = require('config')
-
-// Constants
 const PORT = 8080
 const HOST = '0.0.0.0'
 
-// Configuration from config.js
-const val1 = config.get('Server.key1')
-const val2 = config.get('Server.key2')
+// External Config 샘플 (config.js)
+const config = require('config')
+const configTest = config.get('Server.testKey')
+
+// AWS KMS Decrypt 샘플
+const kmsKeyID = config.get('Server.kmsKeyID')
+const dbUser = config.get('Server.dbUser')
+const dbPassword = config.get('Server.dbPassword')
 
 var AWS = require('aws-sdk')
 AWS.config.update({ region: 'ap-northeast-2' })
 var kms = new AWS.KMS()
 
-var params = {
-  CiphertextBlob:
-    'lOpTFLT1V7JQpOS2EEsqNs7Mfys/aV6M55MOXYkFO321KzHlZxcWDgv1Ps5Ifo05P6XSnxNN0yzmoS46/10GSAz5GgoVFDHaSZmFDax5RTbXBVGvVpGtfFQg9UwEsiv4tWmK/CyVCp1yPo77kHdP3TIkblLEa5g/k3KC/rPTtGJYOYgO+70lbzrXl/WjdVRWoPciC5cu6lhZd4hmP8VcsMDIGmSvn66V2LA30by53z0Ne3ilRKJ0cI9ztDoYwUYRSHVzVlofSGBRIlF5wH78i4SKsNbTjY9cN9Ph/Coy9YtYQmz4Y9hu7msnt6jvsM+BD7RAVNOFUe7tdA4/VFQUx0WiJdieX26ys1Y9x5CU2pfdOr47SBZ+kS6evK5HoT/BsaCz7QxbOG2MxqC+UEF4f1u342eXqcAgrTmw3CIRZPEQhE5haQma0d7sHvnToRUI4KfUcEL/sDbL6pOfa+EVP4psUgGpBFCkiDzXvTS+OhprQV/m9uxbIL261jvqZIw9C9eeugQxEG0o7jjyMCElZIn4g2xX3n0WkWQfPPTkrS5ebQLLqcZ6JCQkJh7RZcC9XSUF8eGEL8Mff2LgpHsfj7VoCvefRSLxmFZwkL3gAca9OGKHezKMDKKMWllUKv7Ezl69uCyb+LjWfgkJP0C3M+4k3eefHYurKZbApkCuV0M=', // The encrypted data (ciphertext).
-  KeyId:
-    'arn:aws:kms:ap-northeast-2:521651615177:key/6ca496ce-938b-4038-af90-e1614da7535c', // A key identifier for the KMS key to use to decrypt the data.
+async function decrypt(source) {
+  const params = {
+    CiphertextBlob: Buffer.from(source, 'base64'),
+    EncryptionAlgorithm: 'RSAES_OAEP_SHA_256',
+    KeyId: kmsKeyID,
+  }
+  const { Plaintext } = await kms.decrypt(params).promise()
+  return Plaintext.toString()
 }
 
-kms.decrypt(params, function (err, data) {
-  if (err) console.log(err, err.stack) // an error occurred
-  else console.log(data) // successful response
-  /*
-   data = {
-    KeyId: "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab", // The Amazon Resource Name (ARN) of the KMS key that was used to decrypt the data.
-    Plaintext: <Binary String>// The decrypted (plaintext) data.
-   }
-   */
-})
+decrypt(dbUser).then((decoded) => console.log(decoded))
+decrypt(dbPassword).then((decoded) => console.log(decoded))
 
 // App
 const app = express()
@@ -41,4 +37,4 @@ app.get('', (req, res) => {
 })
 
 app.listen(PORT, HOST)
-console.log(`Conf: ${val1} ${val2}, Running on http://${HOST}:${PORT}`)
+console.log(`Config: ${configTest}, Running on http://${HOST}:${PORT}`)
